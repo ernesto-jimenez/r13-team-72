4dbf1ebc6e27a16ddc52e65980de52e32b8dfb12
Belén Albeza <belen.albeza@gmail.com>
2013-10-19 14:42:16 +0100

    ADD compass support

--- app/config.rb
+++ app/config.rb
@@ -6,8 +6,15 @@ configure do
     '7ad377314cd2a6692b25b8fb12a'
   set :public_folder, File.join(settings.root, '..', 'public')
 
-  # sass templates by default in public/stylesheets/sass
+  # sass templates in public/stylesheets/sass"
   use Sass::Plugin::Rack
+  Compass.configuration do |c|
+    c.sass_dir = File.join(settings.public_folder, 'stylesheets', 'sass')
+    c.css_dir = File.join(settings.public_folder, 'stylesheets')
+    c.preferred_syntax = :sass
+    c.project_path = settings.public_folder
+  end
+  Compass.configure_sass_plugin!
 
   # partials plugin
   set :partial_template_engine, :erb
