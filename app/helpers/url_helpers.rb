51d810cdc8492cc1ec4e042f5dbe25128bad4b78
Ernesto Jiménez <me@ernesto-jimenez.com>
2013-10-20 19:53:38 +0100

    github url helpers

--- app/helpers/url_helpers.rb
+++ app/helpers/url_helpers.rb
@@ -8,10 +8,32 @@ helpers do
     "/repos/#{repo.owner}/#{repo.name}/#{commit.sha1}/"
   end
 
+  def github_url(repo)
+    [github_base_url,repo.owner,repo.name].join('/')
+  end
+
+  def github_base_url
+    "https://github.com"
+  end
+
+  def github_repo_commits_by(repo, email)
+    [github_url(repo), "commits?author=#{email}"].join('/')
+  end
+
+  def github_commit_url(commit)
+    [github_url(commit.repository), commit, commit.sha1].join('/')
+  end
+
+  def github_file_url(commit, file, line=nil)
+    url = [github_url(commit.repository), commit, commit.sha1, file].join('/')
+    url << "#L#{line}" if line
+  end
+
   def gravatar_url(email, size=nil)
     require 'digest/md5'
     hash = Digest::MD5.hexdigest(email)
     query_string = size.nil? ? '' : "?s=#{size}"
     image_src = "http://www.gravatar.com/avatar/#{hash}/#{query_string}"
+    return image_src
   end
 end
