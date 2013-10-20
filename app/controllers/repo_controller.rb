667b9fae00af89ca84c1149a3955acba76994ae0
Belén Albeza <belen.albeza@gmail.com>
2013-10-20 17:56:17 +0100

    ADD real contributors to repo page

--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -2,6 +2,7 @@ require_relative '../models/repository.rb'
 
 get '/repos/:user/:name/?' do |user, name|
   @repo = Repository.where(owner: user, name: name).first
+  @report = @repo.repo_report
   raise Sinatra::NotFound if @repo.nil?
   erb :'repo/show'
 end
---
+++ app/helpers/repo_helpers.rb
@@ -0,0 +1,9 @@
+helpers do
+  def contributors_by_score(report)
+    report.author_scores.sort_by{|key, value| value}.reverse.map do |x|
+      {:email => x[0],
+       :score => x[1],
+       :name => report.author_names[x[0]]}
+    end
+  end
+end
